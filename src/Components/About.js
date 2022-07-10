d608ff15e84fd4064befc28ce2fcaec2d6f058d6
Jarvis <jignesh07@gmail.com>
2022-07-10 23:12:16 +0530

    -

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,8 +7,18 @@ const About = () => {
         <h2>About Me</h2>
         <div className="container about">
           <div className="about-me">
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor quod ab nihil deserunt pariatur! Corporis ad, eligendi deleniti veritatis distinctio sapiente blanditiis repudiandae ipsa doloribus neque tempora voluptates rerum dicta ullam qui! Quia, dolore. Repellat ex numquam eligendi quis fugiat aliquid cum minus? Facilis recusandae commodi, culpa quidem nulla voluptatibus?</p>
+            <p>Hello, guys. My name is Jignesh Patel. I'm an IT engineer. 
+              I graduated from the GTU in 2022. I describe myself as a passionate 
+              developer who loves coding, open source, and the web platform. Build 
+              web applications with web development best practices. Transform app 
+              designs into front-end code with HTML, CSS, and JavaScript. Work with 
+              back-end developers to integrate UI components with APIs and databases.
+              I love combining the worlds of logic and creative desing to make eye-catching,
+              accessible, and user-friendly websites. I'm excited to make the leap and continue
+              refining my skill with the right company.
+              </p>
           </div>
+
         </div>
     </section>
   )
