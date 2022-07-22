0dd4300bd3c0d6b71c752f57ee52e95ee163316a
Jarvis <jignesh07@gmail.com>
2022-07-22 13:44:10 +0530

    link

--- src/Components/MyProjects.js
+++ src/Components/MyProjects.js
@@ -5,6 +5,7 @@ import Youtube from '../Images/YouTube-clone.png'
 import Spotify from '../Images/Spotify-clone.png'
 import Calculator from '../Images/Calculator.png'
 import RandomQuotes from '../Images/Random-Quotes.png'
+import searchmovie from '../Images/searchmovie.png'
 
 
 
@@ -15,12 +16,12 @@ const MyProjects = () => {
       <h2>Projects</h2>
       <div className="container projects">
         
-        <Projects title='Spotify-Clone' githubLink='https://github.com' liveLink='https://spotify-cone-jp.herokuapp.com/' image={Spotify}/>
-        <Projects title='Youtube-Clone' githubLink='https://github.com' liveLink='https://dribbble.com' image={Youtube}/>
-        <Projects title='Linkedin-Clone' githubLink='https://github.com' liveLink='https://linkedin-clone-jp.herokuapp.com/' image='https://akm-img-a-in.tosshub.com/indiatoday/images/bodyeditor/201904/linkden_9-770x592.JPG?4w0pyK5g0Ea5EMfj3ftAXy2VB4RHU52h'/>
-        <Projects title='Screen-Share-App' githubLink='https://github.com' liveLink='https://dribbble.com' image='https://cdn.dribbble.com/users/2515704/screenshots/18289581/media/f46901c40db1b60dca0cfbd1a768cc0e.jpg?compress=1&resize=400x300&vertical=top'/>
+        <Projects title='Youtube-Clone' githubLink='https://github.com' liveLink='https://youtubeclonejp.netlify.app/explore' image={Youtube}/>
+        <Projects title='Search-Movie' githubLink='https://github.com' liveLink='https://searchmoviejp.netlify.app/' image={searchmovie}/>
         <Projects title='Calculator' githubLink='https://github.com' liveLink='https://calculator-jp.herokuapp.com/' image={Calculator}/>
         <Projects title='Random_Quotes' githubLink='https://github.com' liveLink='https://quote-generate-jp.herokuapp.com/' image={RandomQuotes}/>
+        <Projects title='Linkedin-Clone' githubLink='https://github.com/jarvis311/Linkedin-clone-react' liveLink='https://github.com/jarvis311/Linkedin-clone-react' image='https://akm-img-a-in.tosshub.com/indiatoday/images/bodyeditor/201904/linkden_9-770x592.JPG?4w0pyK5g0Ea5EMfj3ftAXy2VB4RHU52h'/>
+        <Projects title='Spotify-Clone' githubLink='https://github.com/jarvis311/Spotify-clone' liveLink='https://github.com/jarvis311/Spotify-clone' image={Spotify}/>
 
       </div>
     </section>
